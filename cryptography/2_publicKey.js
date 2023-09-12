7ed0d2c7e10ecde44bf170509b19df1f79425746
Lee Marreros <lee@dcspark.io>
2023-09-11 22:25:51 -0500

    Adds x and y from public key

--- cryptography/2_publicKey.js
+++ cryptography/2_publicKey.js
@@ -11,6 +11,7 @@ const publicKey = curve.g.mul(llavePrivada);
 console.log("Public Key", publicKey.encode("hex"));
 // Versión no compacta de llave pública
 // 04 9d4b0a9f4cbdeeb35a328a71d19d0f184665017b6c4a77b3e23e8edcbc850921da6a7859df1797ed2dbda698cbd6f16b62be58fd85d05b1bbb3e9547c8f81127
-
+//  X 9d4b0a9f4cbdeeb35a328a71d19d0f184665017b6c4a77b3e23e8edcbc850921
+//  Y                                                                 da6a7859df1797ed2dbda698cbd6f16b62be58fd85d05b1bbb3e9547c8f81127
 console.log("Public Key (X)", publicKey.getX().toString(16));
 console.log("Public Key (Y)", publicKey.getY().toString(16));
